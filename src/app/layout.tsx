acdfc2a481e9b7c05f8a2cf43ba1cdda38b553b2
Yohan Mayorga <yohanucv@gmail.com>
2024-02-09 16:02:25 -0400

    new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Orbitron } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Orbitron({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Yohan Mayorga",
